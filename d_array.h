07390cf174fded96a260f80e263cf7305d43a971
dheermrt <dheer968793@gmail.com>
2026-07-29 18:48:07 +0530

    Basic vector implementation

---
+++ d_array.h
@@ -0,0 +1,43 @@
+
+#ifndef D_ARR
+#define D_ARR
+#include<stdlib.h>
+#include<string.h>
+typedef struct vector vector;
+struct vector{
+    void* data; 
+    size_t size; 
+    size_t max_cap;
+    size_t size_single;
+    void(*push_back)(vector*,void *);
+    void* (*get)(vector*,int); 
+};
+
+void push_back(vector* v,void* val){
+    if(v->size==v->max_cap){
+        v->data=realloc(v->data,v->max_cap*2);
+        v->max_cap=v->max_cap*2; 
+    }
+    memcpy((char *)v->data + v->size * v->size_single, val, v->size_single);
+    v->size++; 
+    return; 
+}
+void* get(vector* v,int index){
+    if(index>v->size){return NULL;}
+    void* ans=(void *)((char*)v->data+(v->size_single*index)); 
+    return ans; 
+}
+void* init(size_t size_single,size_t max_cap){
+    if(max_cap==0){
+        max_cap=50; 
+    }
+    vector* v=(vector*)(malloc(sizeof(vector))); 
+    v->data=malloc(size_single*max_cap); 
+    v->size=0; 
+    v->max_cap=max_cap;
+    v->size_single=size_single; 
+    v->push_back=push_back; 
+    v->get=get;
+    return (void *)v;
+}
+#endif 
