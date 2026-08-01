276ba0298c1616ab8bf678a2b5eaaf2f5e3f972c
dheermrt <dheer968793@gmail.com>
2026-08-01 16:25:21 +0530

    Fixed indexing bug

--- include/priority_queue.h
+++ include/priority_queue.h
@@ -11,8 +11,8 @@ do{\
 
 #define p_queue_init(ptr,maxer) do{\
 vec_init(ptr,50);\
-ptr[0]=maxer; \
-ptr=ptr+1;   \
+vec_push_back(ptr,maxer);\
+ptr=ptr+1;\
 }while(0);
 
 #define p_push(ptr,val)\
@@ -21,7 +21,7 @@ do{\
     vec_push_back(ptr,val);\
     ptr++; \
     \
-    int i=ptr[-2]-1;\
+    int i=ptr[-2]-2;\
     while(i>=0){\
         int t=(i-1)/2;\
         if(t<0){break;}\
@@ -48,24 +48,24 @@ do{\
     } \
 }while(0); 
 #define p_pop(ptr) do{\
-    ptr[0]=ptr[ptr[-2]-1];\
+    ptr[0]=ptr[ptr[-2]-2];\
     ptr[-2]--;\
     int i=0;\
     if(ptr[-1]==0){\
-        while(i<ptr[-2]){\
+        while(i<(ptr[-2]-1)){\
             int i1=(2*i)+1; \
             int i2=(2*i)+2;\
-            if(i1>=ptr[-2]){\
+            if(i1>=(ptr[-2]-1)){\
                 break;\
             }\
-            else if(i1<ptr[-2] && i2>=ptr[-2]){\
+            else if(i1<ptr[-2] && i2>=(ptr[-2]-1)){\
                 if(ptr[i]>ptr[i1]){\
                     SWAP(ptr[i],ptr[i1]);\
                 }\
                 break;\
             }\
             else{\
-                if(ptr[i1]<=ptr [i2]){\
+                if(ptr[i1]<=ptr[i2]){\
                     if(ptr[i1]<ptr[i]){SWAP(ptr[i],ptr[i1]);i=i1;}\
                     else{\
                         break;\
@@ -81,13 +81,13 @@ do{\
         }\
     }\
     else{\
-        while(i<ptr[-2]){\
+        while(i<(ptr[-2]-1)){\
             int i1=(2*i)+1; \
             int i2=(2*i)+2;\
             if(i1>=ptr[-2]){\
                 break;\
             }\
-            else if(i1<ptr[-2] && i2>=ptr[-2]){\
+            else if(i1<ptr[-2] && i2>=(ptr[-2]-1)){\
                 if(ptr[i]<ptr[i1]){\
                     SWAP(ptr[i],ptr[i1]);\
                 }\
