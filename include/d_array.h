64c47925b6080c5a813cdd84f476fd3037a42dc6
dheermrt <dheer968793@gmail.com>
2026-08-01 16:07:02 +0530

    THere is some logical bug in priority_queue

--- include/d_array.h
+++ include/d_array.h
@@ -2,28 +2,31 @@
 #ifndef D_ARR
 #define D_ARR
 #include<stdlib.h>
+/*
+The size and capacity values are stored in the array itself 
+so you'll have to take care of truncation for example for 
+if you have a char array of size greater than 128/256
+*/
  
- 
-#define vec_push_back(ptr,val)\
-do{\
-    if(*(ptr+ptr[-1])==ptr[-2]){\
+#define vec_push_back(ptr,val) do{\
+    if(ptr[-1]==ptr[-2]){\
         ptr=ptr-2; \
         ptr=(typeof(*ptr)*)(realloc(ptr,sizeof(typeof(*ptr))*(ptr[0]*2))); \
         ptr[0]=ptr[0]*2; \
         ptr=ptr+2;\
     }\
-    *(ptr+ptr[-1])=val; \
-    (ptr[-1])++;\
-}\
-while(0); 
+    ptr[ptr[-1]]=val; \
+    ptr[-1]++;\
+}while(0); 
+
 #define vec_pop_back(ptr)\
-{\
+do{\
     ptr[-1]--; \
     if(ptr[-1]==0){\
         ptr=ptr-2;\
         free(ptr); \
     }\
-}\
+}while(0); 
  
  
 #define front(ptr)\
@@ -32,7 +35,7 @@ ptr[0]
 #define back(ptr)\
 *(ptr+ptr[-1]-1)
 
-#define vec_init(capacity,ptr)\
+#define vec_init(ptr,capacity)\
 do{\
     ptr=(typeof(*ptr)*)(malloc(sizeof(typeof(*ptr))*(capacity+2))); \
     ptr[0]=capacity; \
--- include/priority_queue.h
+++ include/priority_queue.h
@@ -8,73 +8,71 @@ do{\
     a=b;\
     b=temp;\
 }while(0)
-typedef struct priority_queue p_queue; 
-struct priority_queue{
-    vector* v;
-    int maxer;
-};
-static inline p_queue* p_queue_init(size_t size_single,int maxer){
-    p_queue* p=(p_queue*)(malloc(sizeof(p_queue)));
-    p->v=vec_init(size_single,50);
-    p->maxer=maxer;
-    return p;
-}
-#define p_push(p,val)\
+
+#define p_queue_init(ptr,maxer) do{\
+vec_init(ptr,50);\
+ptr[0]=maxer; \
+ptr=ptr+1;   \
+}while(0);
+
+#define p_push(ptr,val)\
 do{\
-    vec_push_back(p->v,val);\
+    ptr--; \
+    vec_push_back(ptr,val);\
+    ptr++; \
     \
-    int i=(int)p->v->size-1;\
+    int i=ptr[-2]-1;\
     while(i>=0){\
         int t=(i-1)/2;\
         if(t<0){break;}\
-        if(p->maxer==0){\
-            if(p->v[t]<=p->v->data[i]){\
+        if(ptr[-1]==0){\
+            if(ptr[t]<=ptr[i]){\
                 break;\
             }\
             else{\
-                SWAP(p->v[t],p->v->data[i]);\
+                SWAP(ptr[t],ptr[i]);\
                 \
                 i=t;\
             }\
         }\
         else{\
-            if(p->v[t]>=p->v->data[i]){\
+            if(ptr[t]>=ptr[i]){\
                 break;\
             }\
             else{\
-                SWAP(p->v[t],p->v->data[i]);\
+                SWAP(ptr[t],ptr[i]);\
                 \
                 i=t;\
             }\
         }\
     } \
 }while(0); 
-#define p_pop(p) do{\
-    p->v[0]=p->v[size-1];\
-    p->v->size--;\
-    int i=0; \
-    if(p->maxer==0){\
-        while(i<p->v->size){\
+#define p_pop(ptr) do{\
+    ptr[0]=ptr[ptr[-2]-1];\
+    ptr[-2]--;\
+    int i=0;\
+    if(ptr[-1]==0){\
+        while(i<ptr[-2]){\
             int i1=(2*i)+1; \
             int i2=(2*i)+2;\
-            if(i1>=p->v->size){\
+            if(i1>=ptr[-2]){\
                 break;\
             }\
-            else if(i1<p->v->size && i2>=p->v->size){\
-                if(p->v->data[i]>p->v->data[i1]){\
-                    SWAP(p->v->data[i],p->v->data[i1]);\
-                    break;\
+            else if(i1<ptr[-2] && i2>=ptr[-2]){\
+                if(ptr[i]>ptr[i1]){\
+                    SWAP(ptr[i],ptr[i1]);\
                 }\
+                break;\
             }\
             else{\
-                if(p->v->data[i1]<=p->v->data[i2]){\
-                    if(p->v->data[i1]<p->v->data[i]){SWAP(p->v->data[i],p->v->data[i1]);i=i1;}\
+                if(ptr[i1]<=ptr [i2]){\
+                    if(ptr[i1]<ptr[i]){SWAP(ptr[i],ptr[i1]);i=i1;}\
                     else{\
                         break;\
                     }\
                 }\
                 else{\
-                    if(p->v->data[i2]<p->v->data[i]){SWAP(p->v->data[i],p->v->data[i2]);i=i2;}\
+                    if(ptr[i2]<ptr[i]){SWAP(ptr[i],ptr[i2]);i=i2;}\
                     else{\
                         break; \
                     }    \
@@ -83,31 +81,34 @@ do{\
         }\
     }\
     else{\
-        int i1=(2*i)+1; \
-        int i2=(2*i)+2;\
-        if(i1>=p->v->size){\
-            break;\
-        }\
-        else if(i1<p->v->size && i2>=p->v->size){\
-            if(p->v->data[i]<p->v->data[i1]){\
-                SWAP(p->v->data[i],p->v->data[i1]);\
+        while(i<ptr[-2]){\
+            int i1=(2*i)+1; \
+            int i2=(2*i)+2;\
+            if(i1>=ptr[-2]){\
                 break;\
             }\
-        }\
-        else{\
-            if(p->v->data[i1]>=p->v->data[i2]){\
-                if(p->v->data[i1]>p->v->data[i]){SWAP(p->v->data[i],p->v->data[i1]);i=i1;}\
-                else{\
-                    break;\
+            else if(i1<ptr[-2] && i2>=ptr[-2]){\
+                if(ptr[i]<ptr[i1]){\
+                    SWAP(ptr[i],ptr[i1]);\
                 }\
+                break;\
             }\
             else{\
-                if(p->v->data[i2]>p->v->data[i]){SWAP(p->v->data[i],p->v->data[i2]);i=i2;}\
+                if(ptr[i1]>=ptr[i2]){\
+                    if(ptr[i1]>ptr[i]){SWAP(ptr[i],ptr[i1]);i=i1;}\
+                    else{\
+                        break;\
+                    }\
+                }\
                 else{\
-                    break; \
-                }    \
+                    if(ptr[i2]>ptr[i]){SWAP(ptr[i],ptr[i2]);i=i2;}\
+                    else{\
+                        break; \
+                    }    \
+                }\
             }\
         }\
     }\
 }while(0); 
+#define p_top(ptr) ptr[0]
 #endif
