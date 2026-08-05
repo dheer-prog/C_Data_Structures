52086d1af359d72edc68a7b94c7c5743e141137a
dheermrt <dheer968793@gmail.com>
2026-08-05 15:19:12 +0530

    Added a cleaner interface for handling multiple maps

--- include/ptr_hash.h
+++ include/ptr_hash.h
@@ -1,9 +1,13 @@
-#ifndef D_PTR
-#define D_PTR
+#ifndef D_ptr
+#define D_ptr
 #include <stdint.h>
 
 #include "d_list.h"
 #define init_bucket_count 20
+
+#define CAT(a,b) a##b
+#define XCAT(a,b) CAT(a,b)
+
 //The hash function being used
 uint64_t splitmix64(uint64_t x)
 {
@@ -12,44 +16,50 @@ uint64_t splitmix64(uint64_t x)
     x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
     return x ^ (x >> 31);
 }
-#define umap_create(type_k,type_v,pair_name,name) \
-typedef struct pair_name{\
-    type_k key;\
-    type_v val;\
-}pair_name;\
-\
-list_create(pair_name,name);\
-typedef struct umap{\
-    name* bucket[init_bucket_count];\
-    pair_name* pair_data_type;\
-    name* list_data_type;\
-    int bucket_count;\
-    int elements;\
-    int elements_threshold;\
-}umap; 
+#define umap_create(name_map,type_k,type_v) \
+typedef struct XCAT(name_map,__pair__){\
+        type_k key;\
+        type_v val;\
+    }XCAT(name_map,__pair__);\
+    list_create(XCAT(name_map,__pair__),XCAT(name_map,__list__));\
+    typedef struct name_map{\
+        XCAT(name_map,__list__)* bucket[init_bucket_count];\
+        XCAT(name_map,__pair__)* pair_data_type;\
+        XCAT(name_map,__list__)* list_data_type;\
+        int bucket_count;\
+        int elements;\
+        int elements_threshold;\
+    }name_map;\
+
 
 #define umap_init(ptr) do{\
-    ptr=(umap *)(malloc(sizeof(umap)));\
+    ptr=(typeof(*ptr) *)(malloc(sizeof(typeof(*ptr))));\
     ptr->pair_data_type=(typeof(*(ptr->pair_data_type))*)NULL;\
     ptr->list_data_type=(typeof(*(ptr->list_data_type))*)NULL;\
     ptr->bucket_count=init_bucket_count;\
     ptr->elements=0;\
     ptr->elements_threshold=(3/4)*(ptr->bucket_count);\
     for(int i=0;i<ptr->bucket_count;i++){\
-        list_init(ptr->bucket[i]); \
+        list_init(ptr->bucket[i]);\
     }\
 }while(0); 
 
 #define umap_insert(u,k,v) \
 do{\
     if(__builtin_types_compatible_p(typeof(k), typeof(int))){\
-        uint64_t h=splitmix64(k); \
-        int index=h%(u->bucket_count); \
-        typeof(*(u->pair_data_type)) t={k,v};\
-        insert_list(u->bucket[index],t);\
+        uint64_t h=splitmix64(k);\
+        int index=h%(u->bucket_count);\
+        typeof(*(u->pair_data_type)) __temp_inserter={k,v};\
+        insert_list(u->bucket[index],__temp_inserter);\
         u->elements++;\
     }\
 }while(0); 
+
+// static inline re_hash(umap* u){
+//     printf("Rehasshing\n");
+//     umap* new_u; 
+//     umap_init(ptr); 
+// }
 
 
 #define umap_size(u) u->elements
@@ -73,22 +83,22 @@ do{\
     }\
 }while(0);
 
-#define umap_get(u,k) ({ \
-    typeof((u)->pair_data_type->val) _result = (typeof((u)->pair_data_type->val)){0}; \
-    int _found = 0; \
-    if (__builtin_types_compatible_p(typeof(k), typeof(int))) { \
-        uint64_t _h = splitmix64(k); \
-        int _index = _h % ((u)->bucket_count); \
-        typeof((u)->list_data_type) _root = (u)->bucket[_index]; \
-        while (_root != NULL) { \
-            if (_root->data.key == (k)) { \
-                _result = _root->data.val; \
-                _found = 1; \
-                break; \
-            } \
-            _root = _root->next; \
-        } \
-    } \
-    _result; \
+#define umap_get(u,k) ({\
+    typeof((u)->pair_data_type->val) _result = (typeof((u)->pair_data_type->val)){0};\
+    int _found = 0;\
+    if (__builtin_types_compatible_p(typeof(k), typeof(int))) {\
+        uint64_t _h = splitmix64(k);\
+        int _index = _h % ((u)->bucket_count);\
+        typeof((u)->list_data_type) _root = (u)->bucket[_index];\
+        while (_root != NULL) {\
+            if (_root->data.key == (k)) {\
+                _result = _root->data.val;\
+                _found = 1;\
+                break;\
+            }\
+            _root = _root->next;\
+        }\
+    }\
+    _result;\
 })
 #endif
