c715ec29e0bbcfc29ea1fe4ecbf1c01a7f1c2efb
dheermrt <dheer968793@gmail.com>
2026-08-17 20:12:47 +0530

    Fixed key val discreprancy

--- include/ordered_hash.h
+++ include/ordered_hash.h
@@ -5,8 +5,8 @@
 #define CAT(a,b) a##b
 #define XCAT(a,b) CAT(a,b)
 #endif 
-typedef unsigned char uchar;
 
+typedef unsigned char uchar;
 #define left_rotate(x) do{\
     typeof(*x)* y=x; \
     x->right=y->left; \
@@ -46,21 +46,22 @@ typedef unsigned char uchar;
     x->parent=y;\
 }while(0); 
 
-#define rb_tree_create(name_rb,type_k,type_v) do{\
+#define rb_tree_create(name_rb,type_k,type_v) ({\
 typedef struct XCAT(name_map,__pair__){\
     type_k key;\
     type_v val;\
 }XCAT(name_rb,__pair__);\
 typedef struct name_rb{\
-    XCAT(name_rb,__pair__);\
+    XCAT(name_rb,__pair__) data;\
     rb_tree* left; \
     rb_tree* right; \
     rb_tree* parent;\
     rb_tree* root; 
     uchar color;    \
     uchar init;\
+    size_t count;\
 }name_rb;\
-}while(0);
+create_ptr_checker();})\
  
 #define rb_init(ptr)do{\
     ptr=(typeof(*ptr)*)(malloc(sizeof(typeof(*ptr)))); 
@@ -73,6 +74,7 @@ typedef struct name_rb{\
     //1 means black
     ptr->init=0;\
     ptr->is_root=1;\
+    ptr>count=0;\
 }while(0);
 
 #define __r_b_property__(ptr)do{\
@@ -110,13 +112,33 @@ typedef struct name_rb{\
     ptr->root->color=1;\
 }while(0);
 
+//Note this returns an iterator like C++ std::map .find() function
+//TODO: Make this change in ptr_hash.h also
+#define map_get(ptr,val) ({
+    typeof(*ptr)* __result__=ptr->root;
+    while(__result__!=NULL && __result__->data!=val){
+        if(__result__->data.key<val){
+            __result__=__result__->right;
+        }
+        else{
+            __result__=__result__->left; 
+        }
+    }
+    __result__;\
+})
+
 #define __bst__insert(ptr,val,val_val)do{\
+    typeof(*ptr)* __finder_iter__=map_get(ptr,val);\
+    if(ptr!=NULL){\
+        ptr->count++;\
+        break;\
+    }\
     if(ptr->init==0){ptr->data=0;ptr->init=1;break;}\
     typeof(*ptr)* curr=ptr; 
     typeof(*ptr)* prev_ptr=NULL;\
     while(curr!=NULL){\
         prev_ptr=curr; \
-        if(curr->data>=val){\
+        if(curr->data.key>=val){\
             curr=curr->left; \
         }\
         else{\
@@ -125,8 +147,7 @@ typedef struct name_rb{\
     }\
     typeof(*ptr)* __temp_new__=(typeof(*ptr)*)(malloc(sizeof(typeof(*ptr))));\
     //Placing a lot of trust in the user LOL; 
-    __temp_new__->data={val,val_val};\ 
-    __temp_new__->data=val;\
+    __temp_new__->data={val,val_val};\
     __temp_new__->init=1;  \
     __temp_new__->parent=prev_ptr;\
     __temp_new__->color=0;\
@@ -140,20 +161,12 @@ typedef struct name_rb{\
     __r_b_property__(__temp_new__);\
 }while(0);
 
+//TODO Currently this can only take in a value and fix something 
+//I want to give the user's an option to send an ptr or a value 
+//but using the same function so I need someway to check whether 
+//val is a ptr or a actual value of sometype 
+#define bst_erase(val) do{
 
-//Note this returns an iterator like C++ std::map .find() function
-//TODO: Make this change in ptr_hash.h also
-#define map_get(ptr,val) ({
-    typeof(*ptr)* __result__=ptr->root;
-    while(__result__->data!=val){
-        if(__result__->data<val){
-            __result__=__result__->right;
-        }
-        else{
-            __result__=__result__->left; 
-        }
-    }
-    __result__;\
-})
+}while(0); 
 
 #endif 
